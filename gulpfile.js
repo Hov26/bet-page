fec2f0550251d3587f2f29486513471836da71c1
Hov26 <hov.ghevondyan1994@gmail.com>
2020-07-30 22:54:00 +0400

    Fix gulp file, add active classnames for "sport-event-item", add "active" class onClick for list items

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,73 @@
 // Gulp
-var gulp = require('gulp');
-var yarn = require('gulp-yarn');
+var gulp = require("gulp");
+var yarn = require("gulp-yarn");
+const terser = require("gulp-terser");
 
 //Task manager
-gulp.task('yarn', function() {
-  return gulp.src(['./package.json', './yarn.lock'])
-    .pipe(gulp.dest('./dist'))
-    .pipe(yarn({
-      production: true
-    }));
+gulp.task("yarn", function() {
+  return gulp
+    .src(["./package.json", "./yarn.lock"])
+    .pipe(gulp.dest("./dist"))
+    .pipe(
+      yarn({
+        production: true
+      })
+    );
 });
 
+function es() {
+  return gulp
+    .src("./js/main.js")
+    .pipe(terser())
+    .pipe(gulp.dest("build/js"));
+}
+gulp.task("es6", es);
+
 // Templates
-var ejs = require('gulp-ejs');
+var ejs = require("gulp-ejs");
 
 // Stylesheet
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
+var sass = require("gulp-sass");
+var sourcemaps = require("gulp-sourcemaps");
+var autoprefixer = require("gulp-autoprefixer");
 var autoprefixerOptions = {
-  browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+  browsers: ["last 2 versions", "> 5%", "Firefox ESR"]
 };
 
 // Notify
-var notify = require('gulp-notify');
+var notify = require("gulp-notify");
 
 // JS
-var gutil = require('gulp-util')
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
+var gutil = require("gulp-util");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
 
 // Server
-var nodemon = require('gulp-nodemon');
-var browserSync = require('browser-sync');
+var nodemon = require("gulp-nodemon");
+var browserSync = require("browser-sync");
 var reload = browserSync.reload;
 
+gulp.task("default", ["styles", "watch", "js", "es6"]);
 
-gulp.task('default', ['styles', 'watch', 'js']);
+gulp.task("build", ["html", "styles", "js"]);
 
-gulp.task('build', ['html', 'styles', 'js']);
-
-gulp.task('watch', ['browserSync'], function() {
-  gulp.watch('./js/main.js', ['js'], reload);
-  gulp.watch(["./sass/**/*"], ['styles'], reload);
+gulp.task("watch", ["browserSync"], function() {
+  gulp.watch("./js/main.js", ["js"], reload);
+  gulp.watch(["./sass/**/*"], ["styles"], reload);
   gulp.watch(["./views/**/*"], reload);
 });
 
-gulp.task('browserSync', ['nodemon'], function() {
+gulp.task("browserSync", ["nodemon"], function() {
   browserSync.init(null, {
     proxy: "http://localhost:3700" // port of node server
   });
 });
 
-gulp.task('nodemon', function(cb) {
+gulp.task("nodemon", function(cb) {
   var callbackCalled = false;
   return nodemon({
-    script: './server.js'
-  }).on('start', function() {
+    script: "./server.js"
+  }).on("start", function() {
     if (!callbackCalled) {
       callbackCalled = true;
       cb();
@@ -64,45 +75,56 @@ gulp.task('nodemon', function(cb) {
   });
 });
 
-gulp.task('styles', function() {
-  gulp.src('./sass/main.sass')
+gulp.task("styles", function() {
+  gulp
+    .src("./sass/main.sass")
     // .pipe(sourcemaps.init())
-    .pipe(sass({errLogToConsole: true}))
-    .on('error',function (err) { 
-      return notify().write(err)
+    .pipe(sass({ errLogToConsole: true }))
+    .on("error", function(err) {
+      return notify().write(err);
     })
     // .pipe(sourcemaps.write())
     .pipe(autoprefixer())
-    .pipe(gulp.dest('./build/css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+    .pipe(gulp.dest("./build/css"))
+    .pipe(
+      browserSync.reload({
+        stream: true
+      })
+    );
 });
 
-gulp.task('js', function() {
-  return gulp.src([
-      './js/main.js'
-    ])
-    // .pipe(concat('all.min.js'))
-    .pipe(uglify())
-    // .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
-    .on('error',function (err) { 
-      return notify().write(err)
-    })
-    .pipe(gulp.dest('build/js'))
+gulp.task("js", function() {
+  return (
+    gulp
+      .src(["./js/main.js"])
+      // .pipe(concat('all.min.js'))
+      .pipe(uglify())
+      // .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
+      .on("error", function(err) {
+        return notify().write(err);
+      })
+      .pipe(gulp.dest("build/js"))
+  );
 });
 
-gulp.task('html', function() {
-  return gulp.src('./views/*.ejs')
-    .pipe(ejs({}, {}, {
-      ext: '.html'
-    }))
-    .pipe(gulp.dest('build'))
+gulp.task("html", function() {
+  return gulp
+    .src("./views/*.ejs")
+    .pipe(
+      ejs(
+        {},
+        {},
+        {
+          ext: ".html"
+        }
+      )
+    )
+    .pipe(gulp.dest("build"));
 });
 
-    // .on('error', notify.onError(function(err) {
-    //   return{
-    //     title:'Styles',
-    //     message:err.message
-    //   };
-    // })
+// .on('error', notify.onError(function(err) {
+//   return{
+//     title:'Styles',
+//     message:err.message
+//   };
+// })
--- js/main.js
+++ js/main.js
@@ -3,10 +3,13 @@ const toggleParentItem = document.getElementsByClassName("toggle-nextSibling");
 function toggleList(e) {
   e.preventDefault();
   let currentList = this.nextElementSibling;
+  let currentItem = this;
   if (currentList.classList.contains("hide")) {
     currentList.classList.remove("hide");
+    currentItem.classList.add("active");
   } else {
     currentList.classList.add("hide");
+    currentItem.classList.remove("active");
   }
 }
 
